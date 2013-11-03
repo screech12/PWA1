a798777cc8959d55486ae382a35a68ac6fa17016
steven creech <screech12@fullsail.edu>
2013-11-03 12:41:39 -0500

    strings
    completed follow long

--- wk1/goal2_practice/js/main.js
+++ wk1/goal2_practice/js/main.js
@@ -158,37 +158,61 @@ var str2 = 'JavaScript loves me!';
 var abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
 // Use charAt() to console.log the 'v' in str1.
+    console.log(str1.charAt(4));
+    //v
 
 
 // Use indexOf() to console.log the index of 'm' in str2.
+    console.log(str2.indexOf('m'));
+    //17
 
 
 // Use lastIndexOf() to console.log the last 'a' in str1.
+    console.log(str1.lastIndexOf('a'));
+    //10
 
 
 // Use length to console.log the length in str1.
+    console.log(str1.length);
+    //19
 
 
 // Use split() to console.log an array of the ABC's using the abc variable.
 // EX: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
+    console.log(abc.split(''));
+
 
 //STUDENT ACTIVITY 1:
 // Use substr() to console.log 'JavaScript!' from str1.
 
+        console.log(str1.substr(7,11));//start at 7 than including the next 11
+
+
 
 // Use concat() to console.log 'I love JavaScript ! JavaScript loves me!' with str1 and str2.
+    console.log(str1.concat(str2));
+    //I love JavaScript! JavaScript loves me!
 
 
 
 //STUDENT ACTIVITY 2:
 // Use substring() to console.log 'JavaScript' from str2.
-
+    console.log(str2.substring(0,10));
+    //JavaScript
 
 //STUDENT ACTIVITY 3:
 // 1. Use toLowerCase() to console.log the str1 all in lowercase letters.
 // 2. Use toUpperCase() to console.log the str1 all in uppercase letters.
 
+    console.log(str1.toLowerCase());
+    console.log(str1.toUpperCase());
+    //i love javascript!
+    //I LOVE JAVASCRIPT!
+
+//Use toString() to console log the type of str1.
+    console.log(typeof str1.toString());
+    //string 
 
 
 /*
@@ -423,7 +447,7 @@ console.log('------ While / Loop ----------');
 	FOR LOOP: for()
 		- a for loop is an increment-based loop, where the increment is 
 			part of the condition
-		- there are 3 statements inside the condition of a for loop: 
+		- there are 3 statements inside the condition of a for loop; 
 			a.  first a counter variable is initialized
 			b.  second the “condition check”
 			c.  and third, increment the variable and perform the code 
