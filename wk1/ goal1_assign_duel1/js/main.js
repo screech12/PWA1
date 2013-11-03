cb501486c55a54d35650985a200d25ed1e3ea8ca
steven creech <screech12@fullsail.edu>
2013-11-03 00:57:44 -0400

    player
    setup players name

--- wk1/ goal1_assign_duel1/js/main.js
+++ wk1/ goal1_assign_duel1/js/main.js
@@ -9,4 +9,18 @@
 
     console.log('FIGHT!!!')
 
+    //players name
+    var playerOneName = 'Spiderman';
+    var playerTwoName = 'Batman';
+
+    //player damage
+    var player1Damage = 20;
+    var player2Damage = 20;
+
+    //player health
+    var playerOneHealth = 100;
+    var playerTwoHealth = 100;
+
+    var round = 1;
+
 })();
