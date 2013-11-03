c38b60d38c318ced212d72b21b8332fd5735bfc4
steven creech <screech12@fullsail.edu>
2013-11-03 13:52:26 -0500

    arrays
    more arrays

--- wk1/goal2_practice/js/main.js
+++ wk1/goal2_practice/js/main.js
@@ -296,14 +296,26 @@ var arr3 = ['apple', 'orange', 50393, 7.324];
 //Returns the first (least) index of an element within the array equal
 //to the specified value, or -1 if none is found.
 
+    console.log(arr2.indexOf('c'));
+    //2
+
 
 // join()
 // Joins all elements of an array into a string.
+
+    console.log(arr2.join(','));
+    //a,b,c,d,e
 
 
 // pop()
 // Removes the last element from an array and returns that element
 // shift() removes the first element from an array.
+
+    var element = arr1.pop();
+    console.log(element);
+    console.log(arr1);
+    //7
+    //[1, 2, 3, 4, 5, 6] (7 was removed from array)
 
 
 // push()
@@ -311,18 +323,37 @@ var arr3 = ['apple', 'orange', 50393, 7.324];
 //   length of the array
 // unshift() - Adds one or more elements to the front of an array.
 
+    arr2.push('f')
+    console.log(arr2);
+    //["a", "b", "c", "d", "e", "f"] (f was added)
+
 
 // reverse()
 // Reverses the order of the elements of an array
+
+    console.log(arr1.reverse());
+    //[6, 5, 4, 3, 2, 1]
 
 
 // splice()
 // Adds and/or removes elements from an array.
 
+    console.log(arr1);
+    console.log(arr1.splice(4, 2)) //NOTE: the array was reversed above
+    console.log(arr1);
+    //[6, 5, 4, 3, 2, 1]
+    //[2, 1]
+    //[6, 5, 4, 3]
 
 // forEach()
 // Calls a function for each element in the array.
 
+    console.log(arr1);
+    arr1.forEach(function(element, index, array){
+        console.log('element', element);
+        console.log('index', index);
+        console.log('array', array);
+    });
 
 
 
