06bc9e19c6ea1a062ff7886208b610c66809ec95
steven creech <screech12@fullsail.edu>
2013-11-03 02:28:45 -0500

    conditional
    determin if there is a winner and changed round at 0

--- wk1/ goal1_assign_duel1/js/main.js
+++ wk1/ goal1_assign_duel1/js/main.js
@@ -21,7 +21,7 @@
     var playerOneHealth = 100;
     var playerTwoHealth = 100;
 
-    var round = 1;
+    var round = 0;
 
     function fight (){
         console.log('in the fight function');
@@ -47,6 +47,14 @@
             var results = winnerCheck();
             console.log(results);
 
+            if(results ==='no winner'){
+                round++;
+                alert(playerOneName+":"+playerOneHealth+"  *ROUND "+round +" OVER"+"*  "+playerTwoName+":"+playerTwoHealth);
+                }else{
+                    alert(results);
+                    break;
+                };
+            };
 
     };
 
@@ -62,7 +70,7 @@
     };
         return result;
     };
-};
+
 
 
 
