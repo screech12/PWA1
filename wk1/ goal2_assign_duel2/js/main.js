9051840408b2b13f4faec67369c97dbe578c674c
steven creech <screech12@fullsail.edu>
2013-11-04 14:48:48 -0500

    Array
    Completed applying array and creatd file jSpractice

--- wk1/ goal2_assign_duel2/js/main.js
+++ wk1/ goal2_assign_duel2/js/main.js
@@ -9,19 +9,7 @@
 
     console.log('FIGHT!!!');
 
-    //players name
-    //var playerOneName = 'Spiderman';
-    //var playerTwoName = 'Batman';
 
-
-    //player damage
-    //var player1Damage = 50;
-    //var player2Damage = 50;
-
-
-    //player health
-//    var playerOneHealth = 100;
-//    var playerTwoHealth = 100;
     var fighter1 = ['Spiderman',50,100];
     var fighter2 = ['Batman',50,100];
 
@@ -31,40 +19,32 @@
         console.log('in the fight function');
 
 
-       // alert(playerOneName+":"+playerOneHealth+" *START*   "+playerTwoName+":"+playerTwoHealth);
-
         alert(fighter1[0]+":"+fighter1[2]+" *START*   "+fighter2[0]+":"+fighter2[2]);
 
         for (var i = 0; i < 10; i++){
 
             //random formula is - Math.floor(Math.random() * (max - min) + min);
-//            var minDamage1 = player1Damage * .5;
-//            var minDamage2 = player2Damage * .5;
-
             var minDamage1 = fighter1[1] * .5;
             var minDamage2 = fighter2[1] * .5;
-//            var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1);
-//            var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
-            var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1);
-            var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
+
+            var f1 = Math.floor(Math.random()*(fighter1[1]-minDamage1)+minDamage1);
+            var f2 = Math.floor(Math.random()*(fighter2[1]-minDamage2)+minDamage2);
 
             //console.log(f1);
             //console.log(f2);
 
             //inflict damage
-//            playerOneHealth-=f1;
-//            playerTwoHealth-=f2;
             fighter1 [2]-=f1;
             fighter2 [2]-=f2;
 
-            console.log(playerOneName+": "+playerOneHealth+"     "+playerTwoName+":"+playerTwoHealth);
+            console.log(fighter1[0]+": "+fighter1 [2]+"     "+fighter2[0]+":"+fighter2 [2]  );
             var results = winnerCheck();
             console.log(results);
 
 
             if(results ==='no winner'){
                 round++;
-                alert(playerOneName+":"+playerOneHealth+" *ROUND "+round +"  OVER"+"*  "+playerTwoName+":"+playerTwoHealth);
+                alert(fighter1[0]+":"+fighter1 [2]+" *ROUND "+round +"  OVER"+"*  "+fighter2[0]+":"+fighter2[2]);
                 }else{
                     alert(results);
                     break;
@@ -78,13 +58,13 @@
 
         var result ='no winner';
 
-    if (playerOneHealth<1 && playerTwoHealth<1){
+    if (fighter1 [2]<1 && fighter2 [2]<1){
         result = "You Both Die";
 
-    }else if(playerOneHealth<1){
-        result = playerTwoName+" WINS!!!";
-    }else if (playerTwoHealth<1){
-        result = playerOneName+" WINS!!!";
+    }else if(fighter1 [2]<1){
+        result = fighter2[0]+" WINS!!!";
+    }else if (fighter2 [2]  <1){
+        result = fighter1[0]+" WINS!!!";
     };
         return result;
     };
