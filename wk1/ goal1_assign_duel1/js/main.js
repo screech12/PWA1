e9f9ad8fb77aa1b9675663eea349e62b800af527
steven creech <screech12@fullsail.edu>
2013-12-01 23:32:31 -0500

    setup fighter
    setup player 1 and two, damage and health

--- wk1/ goal1_assign_duel1/js/main.js
+++ wk1/ goal1_assign_duel1/js/main.js
@@ -7,5 +7,19 @@
 // self-executing function
 (function(){
 
+        console.log("FIGHT!!!");
+
+    //player name
+    var playerOneName = "Spiderman";
+    var playerTwoName = "Batman";
+
+    //player damage
+    var player1damage = 20;
+    var player2damage = 20;
+
+    //player health
+    var playerOneHealth = 100;
+    var playerTwoHealth = 100;
+
 
 })();
