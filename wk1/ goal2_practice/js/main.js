26e779ebe43374f81fba4259cebfe057e2f3101a
steven creech <screech12@fullsail.edu>
2013-12-03 00:58:19 -0500

    strings
    examples compled

--- wk1/ goal2_practice/js/main.js
+++ wk1/ goal2_practice/js/main.js
@@ -179,20 +179,27 @@ var abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
 //STUDENT ACTIVITY 1:
 // Use substr() to console.log 'JavaScript!' from str1.
+    console.log(str1.substr(7,11));//JavaScript!
 
 
 // Use concat() to console.log 'I love JavaScript ! JavaScript loves me!' with str1 and str2.
+    console.log(str1.concat(str2)); //I love JavaScript! JavaScript loves me!
 
 
 
 //STUDENT ACTIVITY 2:
 // Use substring() to console.log 'JavaScript' from str2.
+    console.log(str2.substring(0, 10));//JavaScript
 
 
 //STUDENT ACTIVITY 3:
 // 1. Use toLowerCase() to console.log the str1 all in lowercase letters.
 // 2. Use toUpperCase() to console.log the str1 all in uppercase letters.
+    console.log(str1.toLowerCase()); //i love javascript!
+    console.log(str1.toUpperCase()); //I LOVE JAVASCRIPT!
 
+    //use toString() to console log the type of str1.
+    console.log(typeof str1.toString());//string
 
 
 /*
