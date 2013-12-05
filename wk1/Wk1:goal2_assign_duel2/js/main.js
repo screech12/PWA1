7c7f3fb119d6a70acd25090cfc23509a9569173d
steven creech <screech12@fullsail.edu>
2013-12-04 19:40:02 -0500

    change var
    setup array for var

--- wk1/Wk1:goal2_assign_duel2/js/main.js
+++ wk1/Wk1:goal2_assign_duel2/js/main.js
@@ -10,17 +10,8 @@
         console.log("FIGHT!!!");
 
     //player name
-    var playerOneName = "Spiderman";
-    var playerTwoName = "Batman";
-
-    //player damage
-    var player1Damage = 20;
-    var player2Damage = 20;
-
-    //player health
-    var playerOneHealth = 100;
-    var playerTwoHealth = 100;
-
+    var fighter1 = ['Spiderman',50,100];
+    var fighter2 = ['Batman', 50,100];
     var round = 0;
 
     function fight(){
@@ -31,17 +22,17 @@
        for (var i = 0; i < 10; i++){
          //random formula is - Math.floor(Math.random() * (max - min) + min;
 
-           var minDamage1 = player1Damage * .5;
-           var minDamage2 = player2Damage * .5;
-           var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1);
-           var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
+           var minDamage1 = fighter1 * .5;
+           var minDamage2 = fighter2 * .5;
+           var f1 = Math.floor(Math.random()*(fighter[1]-minDamage1)+minDamage1);
+           var f2 = Math.floor(Math.random()*(fighter[2]-minDamage2)+minDamage2);
 
            //console.log(f1);
            //console.log(f2);
 
            //inflict damage
-           playerOneHealth-=f1;
-           playerTwoHealth-=f2;
+           fighter1 [2]-=f1;
+           fighter2 [2]-=f2;
 
            console.log(playerOneName+":"+playerOneHealth+" "+playerTwoName+":"+playerTwoHealth);
            var results = winnerCheck();
