943c5513913655430bc37120c452cb5d74c3a4ea
steven creech <screech12@fullsail.edu>
2014-02-13 19:54:53 -0500

    mini damaged
    change to array

--- Wk1:goal2_assign_duel2/js/main.js
+++ Wk1:goal2_assign_duel2/js/main.js
@@ -11,31 +11,31 @@
 
     //player name
     var fighter1 = ["Spiderman", 20, 100];
-    var playerTwoName = "Batman";
+    var fighter2 = ["Batman" , 20, 100];
 
     //player damage
     //var player1Damage = 20;
-    var player2Damage = 20;
+    //var player2Damage = 20;
 
     //player health
     //var playerOneHealth = 100;
-    var playerTwoHealth = 100;
+    //var playerTwoHealth = 100;
 
     var round = 0;
 
     function fight(){
         console.log('in the fight function');
 
-        alert(fighter1[0]+":"+fighter1[2]+" *START* "+playerTwoName+":"+playerTwoHealth);
+        alert(fighter1[0]+":"+fighter1[2]+" *START* "+fighter2[0]+":"+fighter2[2]);
 
         for (var i = 0; i < 10; i++){
             //random formula is - Math.floor(Math.random() * (max - min) + min;
             //console.log(i);
 
-            var minDamage1 = player1Damage * .5;
-            var minDamage2 = player2Damage * .5;
-            var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1);
-            var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
+            var minDamage1 = fighter1[1] * .5;
+            var minDamage2 = fighter2[1] * .5;
+            var f1 = Math.floor(Math.random()*(fighter1[1]-minDamage1)+minDamage1);
+            var f2 = Math.floor(Math.random()*(fighter2[2]-minDamage2)+minDamage2);
 
             //console.log(f1);
             //console.log(f2);
